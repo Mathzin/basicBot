ec07eed2a67da9aaf7ced17d3f0cd4bdae8357c3
yemasthui <yemasthui@gmail.com>
2014-08-02 13:21:54 +0200

    1.2.1
    bugfix

--- basicBot.js
+++ basicBot.js
@@ -75,7 +75,7 @@ var retrieveFromStorage = function(){
 };
 
 var esBot = {
-        version: "1.2.0",        
+        version: "1.2.1",        
         status: false,
         name: "basicBot",
         creator: "Matthew",
@@ -826,8 +826,8 @@ var esBot = {
                     case '!roulette':           esBot.commands.rouletteCommand.functionality(chat, '!roulette');                    executed = true; break;
                     case '!rules':              esBot.commands.rulesCommand.functionality(chat, '!rules');                          executed = true; break;
                     case '!sessionstats':       esBot.commands.sessionstatsCommand.functionality(chat, '!sessionstats');            executed = true; break;
-                    case '!skip':               esBot.commands.skipCommand.functionality(chat, '!skip');
-                    case '!slap': esBot.commands.slapCommand.functionality(chat, '!slap'); executed = true; break;                            executed = true; break;
+                    case '!skip':               esBot.commands.skipCommand.functionality(chat, '!skip');                            executed = true; break;
+                    case '!slap':               esBot.commands.slapCommand.functionality(chat, '!slap'); executed = true; break;    executed = true; break;
                     case '!status':             esBot.commands.statusCommand.functionality(chat, '!status');                        executed = true; break;
                     case '!swap':               esBot.commands.swapCommand.functionality(chat, '!swap');                            executed = true; break;
                     case '!theme':              esBot.commands.themeCommand.functionality(chat, '!theme');                          executed = true; break;
